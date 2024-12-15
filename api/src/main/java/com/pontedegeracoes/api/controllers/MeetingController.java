a27c0b314cfd69b61af1f252709048076ba08aaa
Vítor Oliveira <caulicons@gmail.com>
2024-12-15 12:29:23 -0300

    #30 Enhance MeetingController and Meeting entity: add user validation for meeting creation, update meeting details, and include getter for meeting ID

--- api/src/main/java/com/pontedegeracoes/api/controllers/MeetingController.java
+++ api/src/main/java/com/pontedegeracoes/api/controllers/MeetingController.java
@@ -1,7 +1,11 @@
 package com.pontedegeracoes.api.controllers;
 
 import com.pontedegeracoes.api.entitys.Meeting;
+import com.pontedegeracoes.api.entitys.User;
 import com.pontedegeracoes.api.repositories.MeetingRepository;
+import com.pontedegeracoes.api.repositories.UserRepository;
+
+import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,6 +21,9 @@ public class MeetingController {
   @Autowired
   private MeetingRepository meetingRepository;
 
+  @Autowired
+  private UserRepository userRepository;
+
   @GetMapping
   public ResponseEntity<List<Meeting>> getAllMeetings() {
     List<Meeting> meetings = meetingRepository.findAll();
@@ -30,17 +37,30 @@ public class MeetingController {
   }
 
   @PostMapping
-  public ResponseEntity<Meeting> createMeeting(@RequestBody Meeting meeting) {
+  public ResponseEntity<Meeting> createMeeting(@Valid @RequestBody Meeting meeting) {
+
+    /* Check if the sender and recipient exists before save */
+    Optional<User> sender = userRepository.findById(meeting.getSender().getId());
+    Optional<User> recipient = userRepository.findById(meeting.getRecipient().getId());
+
+    if (sender.isEmpty() || recipient.isEmpty()) {
+      return ResponseEntity.notFound().build();
+    }
+
     Meeting savedMeeting = meetingRepository.save(meeting);
     return ResponseEntity.status(HttpStatus.CREATED).body(savedMeeting);
   }
 
   @PutMapping("/{id}")
   public ResponseEntity<Meeting> updateMeeting(@PathVariable Long id, @RequestBody Meeting meetingDetails) {
+
     Optional<Meeting> meeting = meetingRepository.findById(id);
     if (meeting.isPresent()) {
       Meeting updatedMeeting = meeting.get();
       updatedMeeting.setName(meetingDetails.getName());
+      updatedMeeting.setDescription(meetingDetails.getDescription());
+      updatedMeeting.setType(meetingDetails.getType());
+      updatedMeeting.setMessage(meetingDetails.getMessage());
       updatedMeeting.setDate(meetingDetails.getDate());
 
       return ResponseEntity.ok(meetingRepository.save(updatedMeeting));
